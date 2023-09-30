6d7d81c384cd4cc441c56fd1bcef0756d5c8bb6c
Momosan09 <gaelpaezg@gmail.com>
2023-09-30 18:09:36 -0300

    ajustes

--- core/src/com/mygdx/hud/Combinacion.java
+++ core/src/com/mygdx/hud/Combinacion.java
@@ -1,12 +1,8 @@
 package com.mygdx.hud;
 
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.InputProcessor;
 import com.badlogic.gdx.graphics.Color;
-import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.scenes.scene2d.Stage;
-import com.badlogic.gdx.scenes.scene2d.ui.Image;
 import com.badlogic.gdx.scenes.scene2d.ui.Label;
 import com.badlogic.gdx.scenes.scene2d.ui.Table;
 import com.mygdx.utiles.Colores;
@@ -14,7 +10,7 @@ import com.mygdx.utiles.DibujarFiguras;
 import com.mygdx.utiles.EstiloFuente;
 import com.mygdx.utiles.MyDragAndDrop;
 import com.mygdx.utiles.Recursos;
-import com.mygdx.utiles.Render;
+
 import com.mygdx.io.Entradas;
 
 public class Combinacion implements HeadUpDisplay{
@@ -23,23 +19,21 @@ public class Combinacion implements HeadUpDisplay{
 	private Table contenedor;
 	private Label titulo;
 
-    private DibujarFiguras fondo, dondeApuntar, cuadradoRojo;
+    private DibujarFiguras fondo;
     private EstiloFuente estiloFuente;
 	private Label.LabelStyle labelStyle;
     private float ancho=800, alto=700;
     Entradas entradas = new Entradas();
     
-    MyDragAndDrop dp;
+    MyDragAndDrop dragNDrop;
 
     public Combinacion() {
     	
     	fondo = new DibujarFiguras();
-    	cuadradoRojo = new DibujarFiguras();
-    	dondeApuntar = new DibujarFiguras();
         stage = new Stage();
         
-        dp = new MyDragAndDrop(stage);
-        dp.create();
+        dragNDrop = new MyDragAndDrop(stage);
+        dragNDrop.create();
         crearFuentes();
         crearActores();
         poblarStage();
@@ -51,8 +45,7 @@ public class Combinacion implements HeadUpDisplay{
     public void render() {
     	fondo.dibujarRectanguloLleno((Gdx.graphics.getWidth()/2)-(ancho/2), (Gdx.graphics.getHeight()/2)-(alto/2) , ancho, alto, new Color(0,0,0,.7f));
     	 stage.draw();
-    	 dondeApuntar.dibujarRectanguloLleno(490, 490, 10, 10, new Color(0,1,0,1));
-    	 dp.render();
+    	 dragNDrop.render();
     }
     
 	@Override
