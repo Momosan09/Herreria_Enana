a33cfd12931615ad795767320ff792c70813714d
Momosan09 <gaelpaezg@gmail.com>
2023-12-05 20:05:33 -0300

    actualiza ejecutable

--- assets/mapas/exterior.tsx
+++ assets/mapas/exterior.tsx
@@ -1,11 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.10" tiledversion="1.10.2" name="exterior" tilewidth="32" tileheight="32" tilecount="36" columns="6">
  <image source="exterior.png" width="192" height="192"/>
- <tile id="19">
-  <objectgroup draworder="index" id="5">
-   <object id="7" x="0" y="8.84636">
-    <polygon points="0,0 0,23.1536 32,23.1536 32,-0.846358 0,-0.846358"/>
-   </object>
-  </objectgroup>
- </tile>
 </tileset>
--- desktop/build/resources/main/mapas/exterior.tsx
+++ desktop/build/resources/main/mapas/exterior.tsx
@@ -1,10 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.10" tiledversion="1.10.2" name="exterior" tilewidth="32" tileheight="32" tilecount="36" columns="6">
  <image source="exterior.png" width="192" height="192"/>
- <tile id="19">
-  <objectgroup draworder="index" id="5">
-   <object id="4" x="0.03125" y="9.0625" width="32.0313" height="22.875"/>
-   <object id="6" x="0" y="0" width="32" height="32"/>
-  </objectgroup>
- </tile>
 </tileset>
